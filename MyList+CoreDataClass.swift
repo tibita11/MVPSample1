c708152393d0fb64fe11a6da57e896839dba5933
tibita11 <tibita0811@gmail.com>
2023-09-04 11:52:24 +0900

    feat: CoreData導入

---
+++ MyList+CoreDataClass.swift
@@ -0,0 +1,24 @@
+//
+//  MyList+CoreDataClass.swift
+//  MVPSample1
+//
+//  Created by 鈴木楓香 on 2023/09/04.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MyList)
+public class MyList: NSManagedObject {
+    static func new(itemData: ItemData) -> MyList {
+        let entity: MyList = CoreDataRepository.entity()
+        entity.title = itemData.title
+        entity.detail = itemData.description
+        return entity
+    }
+    
+    func update(newName: String) {
+        self.title = newName
+    }
+}
---
+++ MyList+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  MyList+CoreDataProperties.swift
+//  MVPSample1
+//
+//  Created by 鈴木楓香 on 2023/09/04.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension MyList {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MyList> {
+        return NSFetchRequest<MyList>(entityName: "MyList")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var detail: String?
+
+}
+
+extension MyList : Identifiable {
+
+}
